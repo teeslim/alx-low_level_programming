35d8ef54724cad64035d9462c33c51abcd598e37
myUsername <myemail@gmail.com>
2023-03-20 21:55:29 -0700

    UNIX is basically a simple operating system, but you have to be a genius to understand the simplicity

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -4,8 +4,7 @@
  * main - A c program that prints a line to the standard error
  * Return: 1 (Success)
  */
-int main(void);
-
+int main(void)
 {
 fput("and that piece of art is useful\"Dora Korpar, 2015-10-19\n", stdout);
 return (1);
